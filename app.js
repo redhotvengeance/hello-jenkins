60ff21150fbf191dd6eb9b08ca53c9f8a43ab22c
redhotvengeance <rhv@redhotvengeance.com>
2014-07-23 22:25:41 -0700

    Should fail and not deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello jenkinss');
+  res.send('hello jenkinsss');
 });
 
 app.listen(process.env.PORT || 5000);
